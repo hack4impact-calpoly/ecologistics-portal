2fdc0ec32b9d1db7bc017201f9ecb58513ce7b29
KimLinhVu <klvbubble@gmail.com>
2024-01-21 16:54:57 -0800

    feat: add button to homepage that links to new page with name

---
+++ src/app/kimlinh/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-sky-400 text-center text-5xl font-semibold tracking-wide">
+        Kim-Linh Vu
+      </h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <Link href="/kimlinh">
+        <Button variant="outline">Kim-Linh</Button>
+      </Link>
     </main>
   );
 }
