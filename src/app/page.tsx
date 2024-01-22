2cb6c0a20e0cabd9fccb444d34d216e4588625ca
vilinh <vilinhlvu@gmail.com>
2024-01-21 16:31:52 -0800

    feat: button first task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <Button asChild>
+        <Link href="/vi">go to page</Link>
+      </Button>
     </main>
   );
 }
---
+++ src/app/vi/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className="bg-pink-500 text-white p-4">Vi-Linh Vu</div>;
+}
