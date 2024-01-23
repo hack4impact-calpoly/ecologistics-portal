2bc4d0b4944fc1898cb696ffeed4e7176473c102
joieng1 <johncieng1@gmail.com>
2024-01-23 12:22:23 -0800

    feat: Added button to redirect to custom page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <Link href="/johnieng">
+        <Button className="my-2 bg-sky-300 text-black-200">John Ieng</Button>
+      </Link>
     </main>
   );
 }
