26b0444a70d84a14c3c1fcb30dc8f39fe4cb8c77
drdonv <realdrdonv@gmail.com>
2024-01-23 17:08:09 -0800

    feat: added new page, button, and a global css file to control all headings, etc.

---
+++ src/app/dhanviganti/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import "@/global.css";
+
+export default function Me() {
+  return (
+    <main>
+      <div className="text-center">
+        <h1 className="text-black-50 font-bold">Welcome to my page!</h1>
+      </div>
+      <div className="text-center">
+        <p>
+          My name is Dhanvi Ganti, and I am a software developer at CalPoly SLO!
+        </p>
+        <Link href="../">
+          <Button>Go home</Button>
+        </Link>
+      </div>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import "@/global.css";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <Link href="./dhanviganti">
+        <Button>Check me out!</Button>
+      </Link>
     </main>
   );
 }
