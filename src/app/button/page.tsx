a1d3824f76e9902a4ab73d45c9de30b883a9edf0
Peter Chinh <peterchinh1018@gmail.com>
2024-01-26 12:55:36 -0800

    feat: added button that routes to a new page

---
+++ src/app/button/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="bg-purple-200 min-h-screen flex items-center justify-center">
+      <div className="text-6xl text-red-500 text-center font-mono">
+        Peter Chinh
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <Link href="/button/">
+        <Button variant="outline">Button</Button>
+      </Link>
     </main>
   );
 }
