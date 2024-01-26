dd3ff3e68aac2ed348258c1255a19685bc258c07
brandonwDiaryz <legobw1018@gmail.com>
2024-01-26 01:17:59 -0800

    feat: Add button that routes to new page

---
+++ src/app/brandon/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex justify-center">
+      <div className="bg-blue-300 p-4 text-lg">Brandon Wong</div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <Link href="/brandon">
+        <Button variant="outline">Go To Brandon&apos;s Page</Button>
+      </Link>
     </main>
   );
 }
