c0a498fd8ffee5c19cb76b24acdb604ae2a05d60
Taran Singh <tarannssingh@gmail.com>
2024-01-26 19:26:24 -0800

    feat: add button with my name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <Button variant="destructive" asChild>
+        <Link href="/Taran">Taran</Link>
+      </Button>
     </main>
   );
 }
