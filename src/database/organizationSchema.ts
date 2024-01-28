29a7dcf8c72abc2425a3841afcf782ea71035ee2
Marxalpha <saikrishnaknm@gmail.com>
2024-01-28 14:12:17 +0530

    Added Organization Schema

---
+++ src/database/organizationSchema.ts
@@ -0,0 +1,32 @@
+import { Schema, model, models } from "mongoose";
+
+const OrganizationSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required'],
+    },
+    website:{
+        type: String,
+        required: [true, 'Website is required'],
+    },
+    clerkUser:{
+        type: String,
+        required: [true, 'Clerk User is required'],
+    },
+    logo:{
+        type: String,
+        required: [true, 'Logo is required'],
+    },
+    reimbursement:{
+        type: [{ type: Schema.Types.ObjectId, ref: 'Reimbursement' }],
+        required: [true, 'Reimbursement is required'],
+    },
+});
+
+const Organization = models.Organization || model("Organization", OrganizationSchema);
+
+export default Organization;
