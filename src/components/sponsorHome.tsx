910f76083aa66fa0923c73e922e7064a82728f22
joieng1 <johncieng1@gmail.com>
2024-02-04 12:33:15 -0800

    feat: added sponsorHome component

---
+++ src/components/sponsorHome.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function SponsorHome() {
+  return (
+    <div>
+      <h1>Nonprofit Name</h1>
+      <Button>Request Reimbursement</Button>
+      <Button>Edit info</Button>
+    </div>
+  );
+}
