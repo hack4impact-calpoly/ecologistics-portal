9a68120487edeb3bfa59452811d48b11ab55ff3c
Luke Waltz <lukewaltz@outlook.com>
2024-02-09 21:21:44 -0800

    feat: working POST and GET (all) routes. GET (by ID) is not working

--- src/app/api/reimbursement/[id]/route.ts
+++ src/app/api/reimbursement/[id]/route.ts
@@ -18,20 +18,3 @@ export async function GET(req: NextRequest, params: { id: string }) {
     return NextResponse.json("Issue with Get Req", { status: 400 });
   }
 }
-
-// export async function GET(req: NextRequest, params: string) {
-//   await connectDB();
-//   const id = params;
-
-//   try {
-//     const reimburse = await Reimbursement.findById(id);
-//     if (!reimburse) {
-//       return NextResponse.json("Reimbursement Not Found", { status: 404 });
-//       //   return NextResponse.error({ message: "Reimbursement not found" }, 404);
-//     }
-//     return NextResponse.json(reimburse);
-//   } catch (error) {
-//     return NextResponse.json("Issue with Get Req", { status: 400 });
-//     // return NextResponse.error({ message: error.message }, 500);
-//   }
-// }
