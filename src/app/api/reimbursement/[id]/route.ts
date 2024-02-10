79b26e7077160d88b5e4fd9a09773fef1e547daf
Luke Waltz <lukewaltz@outlook.com>
2024-02-09 21:35:01 -0800

    feat: all reimbursement routes working

--- src/app/api/reimbursement/[id]/route.ts
+++ src/app/api/reimbursement/[id]/route.ts
@@ -2,17 +2,23 @@ import connectDB from "@database/db";
 import { NextRequest, NextResponse } from "next/server";
 import Reimbursement from "@database/reimbursementSchema";
 
-export async function GET(req: NextRequest, params: { id: string }) {
+export type IParams = {
+  params: {
+    id: string;
+  };
+};
+
+export async function GET(req: NextRequest, { params }: IParams) {
   await connectDB();
+  const { id } = params; // Extract the id from params
 
   try {
-    const { id } = params; // Extract the id from params
+    console.log(id);
     const reimburse = await Reimbursement.findById(id);
 
     if (!reimburse) {
       return NextResponse.json("Reimbursement Not Found", { status: 404 });
     }
-
     return NextResponse.json(reimburse);
   } catch (error) {
     return NextResponse.json("Issue with Get Req", { status: 400 });
