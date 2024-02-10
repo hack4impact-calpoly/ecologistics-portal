6e8e644fb63482afb70d30117f6adf5684bc9584
vilinh <vilinhlvu@gmail.com>
2024-02-09 16:51:49 -0800

    feat: expanded rows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import ReimbursementRequestsTable from "@/components/reimbursement-table/table";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <ReimbursementRequestsTable />
     </main>
   );
 }
