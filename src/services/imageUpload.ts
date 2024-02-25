2589780e1ee0f811863995f5fbda469d6097afa4
Taran Singh <tarannssingh@gmail.com>
2024-02-24 23:18:56 -0800

    feat: add services file

---
+++ src/services/imageUpload.ts
@@ -0,0 +1,27 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+const client = new S3Client({
+  region: process.env.AWS_S3_LOCATION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY || "",
+    secretAccessKey: process.env.AWS_PRIV_KEY || "",
+  },
+});
+
+export const imageUpload = async (file: any, fileName: string) => {
+  const fileBuffer = file;
+  const command = new PutObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `${fileName}-${Date.now()}`,
+    Body: fileBuffer,
+    ContentType: "image",
+  });
+
+  try {
+    const response = await client.send(command);
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+  }
+  return fileName;
+};
