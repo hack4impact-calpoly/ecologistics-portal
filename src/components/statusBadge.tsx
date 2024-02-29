cda1e1ba698400d53747c4eb5c3d782892ff3a6b
Taran Singh <tarannssingh@gmail.com>
2024-02-28 22:07:22 -0800

    feat: edit badge css

---
+++ src/components/statusBadge.tsx
@@ -0,0 +1,73 @@
+// Next, create a StatusBadge component that takes ReimbursementStatus as a prop and
+// returns the corresponding badge as shown in the figma.
+import Status from "@/lib/enum";
+import { Badge } from "@/components/ui/badge";
+
+export function choosingTheBadge(ReimbursementStatus: Status) {
+  switch (ReimbursementStatus) {
+    case Status.Pending:
+      return (
+        <Badge
+          variant="status"
+          className="bg-blue-200 font-medium text-blue-800"
+        >
+          Pending
+        </Badge>
+      );
+    case Status.NeedsReview:
+      return (
+        <Badge
+          variant="status"
+          className="bg-gray-100 font-medium text-gray-700"
+        >
+          Needs Review
+        </Badge>
+      );
+    case Status.OnHold:
+      return (
+        <Badge
+          variant="status"
+          className="bg-orange-100 font-medium text-orange-900"
+        >
+          On Hold
+        </Badge>
+      );
+    case Status.Declined:
+      return (
+        <Badge variant="status" className="bg-red-100 font-medium text-red-900">
+          Declined
+        </Badge>
+      );
+    case Status.Paid:
+      return (
+        <Badge
+          variant="status"
+          className="bg-green-100 font-medium text-green-900"
+        >
+          Paid
+        </Badge>
+      );
+    case Status.Archived:
+      return (
+        <Badge
+          variant="status"
+          className="bg-gray-700 font-medium text-gray-100"
+        >
+          Archived
+        </Badge>
+      );
+    default:
+      return (
+        <Badge
+          variant="status"
+          className=" bg-orange-400 font-medium text-red-800"
+        >
+          ERROR
+        </Badge>
+      );
+  }
+}
+
+export default function StatusBadge(ReimbursementStatus: Status) {
+  return <div>{choosingTheBadge(ReimbursementStatus)}</div>;
+}
