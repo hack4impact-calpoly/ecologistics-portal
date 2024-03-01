11387d9279b6ccb5bbc799f0f1e9091045e96cee
ktaschek <ktaschek@gmail.com>
2024-03-01 14:33:15 -0800

    feat: added testing auth conditional rendering aproached

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+"use client";
 import ReimbursementCard from "@/components/reimbursement-card";
 import AdminTable from "@/components/admin-table/admin-table";
 import SponsoredOrgTable from "@/components/sponsored-org-table/sponsored-org-table";
 import { data } from "@/test/mock-data";
+import { useUser } from "@clerk/nextjs";
 
 export default function Home() {
+  const { isLoaded, isSignedIn, user } = useUser();
+  if (!isLoaded || !isSignedIn) {
+    return <div>Loading...</div>;
+  }
+  if (!user?.publicMetadata?.admin) {
+    return <div>Unauthorized</div>;
+  }
   return (
     <main>
-      {/* <AdminTable /> */}
+      <AdminTable />
       {/* <SponsoredOrgTable /> */}
       {/* <ReimbursementCard
         {...data[0]}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import { Nav } from "./nav";
 import {
   HomeIcon,
@@ -7,9 +7,65 @@ import {
   PinLeftIcon,
   PinRightIcon,
 } from "@radix-ui/react-icons";
+import { useUser } from "@clerk/nextjs";
+import { set } from "mongoose";
+
+const links = [
+  {
+    title: "Home",
+    route: "/",
+    icon: HomeIcon,
+  },
+  {
+    title: "Sponsored Organizations",
+    route: "/sponsored-organizations",
+    icon: FileTextIcon,
+  },
+  {
+    title: "Submit Request",
+    route: "/reimbursements",
+    icon: FileTextIcon,
+  },
+];
+
+const selectLink = (user: any) => {
+  if (user?.publicMetadata?.admin) {
+    return links.slice(0, 2);
+  } else if (user?.publicMetadata?.organization) {
+    return links[0];
+  } else if (user?.publicMetadata?.organization?.approved) {
+    return links.slice(0, 1).concat(links[2]);
+  } else {
+    return links[0];
+  }
+};
 
 const Sidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(true);
+  const { isLoaded, isSignedIn, user } = useUser();
+  if (!isLoaded || !isSignedIn) {
+    return (
+      <div
+        className={`flex flex-col h-screen bg-[#335543] ${
+          !isCollapsed ? "w-64" : "w-17"
+        }`}
+      >
+        {isCollapsed ? (
+          <PinRightIcon
+            className="h-[45px] w-[45px] text-white mx-auto my-2 hover:bg-gray-200 hover:bg-opacity-50 p-2 rounded-full cursor-pointer"
+            onClick={() => setIsCollapsed(!isCollapsed)}
+          />
+        ) : (
+          <PinLeftIcon
+            className="h-[45px] w-[45px] text-white place-self-end my-2 mr-2 p-2 hover:bg-gray-200 hover:bg-opacity-50 rounded-full cursor-pointer"
+            onClick={() => setIsCollapsed(!isCollapsed)}
+          />
+        )}
+      </div>
+    );
+  }
+
+  const selectedLinks = selectLink(user);
 
   return (
     <div
@@ -28,21 +84,7 @@ const Sidebar = () => {
           onClick={() => setIsCollapsed(!isCollapsed)}
         />
       )}
-      <Nav
-        isCollapsed={isCollapsed}
-        links={[
-          {
-            title: "Home",
-            route: "/",
-            icon: HomeIcon,
-          },
-          {
-            title: "Sponsored Organizations",
-            route: "/sponsored-organizations",
-            icon: FileTextIcon,
-          },
-        ]}
-      />
+      <Nav isCollapsed={isCollapsed} links={links} />
     </div>
   );
 };
