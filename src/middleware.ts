ce2941267f90747bf0c3248b83323406eb4afea2
Taran Singh <tarannssingh@gmail.com>
2024-03-03 15:02:44 -0800

    feat: remove archive and edit file naming for badge

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   //   publicRoutes: ["/"], This is left here as a reminder that we can add pages that don't require auth.
+  // publicRoutes: ["/api/s3-bucket"],
 });
 
 export const config = {
