cff4f02cc8835ef8e8f116c778c041c067f1ac85
iloo <igloo405@gmail.com>
2024-03-02 17:17:51 -0800

    v

--- cypress/e2e/sponsored-org-tests/reimbursement-search-test.cy.ts
+++ cypress/e2e/sponsored-org-tests/reimbursement-search-test.cy.ts
@@ -2,6 +2,8 @@ describe("Sponsor Card Functionality", () => {
   it("let's us view sponsor cards with updates", () => {
     // cy.visit("http://localhost:3000/sponsored-organizations");
     // cy.get('button[value="view-update"]').click({ force: true });
-    cy.visit("http://localhost:3000");
+    cy.visit(
+      "https://quality-goblin-47.accounts.dev/sign-in?redirect_url=http%3A%2F%2Flocalhost%3A3000%2F",
+    );
   });
 });
