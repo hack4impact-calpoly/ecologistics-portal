0b157999cc12bbbd8464cbe3c73a8876e7de800b
RyanHu021 <ryanhu021@gmail.com>
2024-03-04 15:47:12 -0800

    clean up image upload and update env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 "use client";
 import AdminTable from "@/components/admin-table/admin-table";
 import SponsoredOrgTable from "@/components/sponsored-org-table/sponsored-org-table";
+import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
+import { imageUpload } from "@/services/image-upload";
+
+const uploadImageTest = async () => {
+  const file = new File([""], "sponsored_org_profile_picture_placeholder.png", {
+    type: "image/png",
+  });
+  const url = await imageUpload(file, "test");
+  console.log(url);
+};
 
 export default function Home() {
   const router = useRouter();
@@ -18,6 +28,7 @@ export default function Home() {
       <main>
         <h1 className="text-xl font-bold">Nonprofit Name</h1>
         <AdminTable />
+        <Button onClick={uploadImageTest}>Upload Image</Button>
       </main>
     );
   } else {
--- src/services/image-upload.ts
+++ src/services/image-upload.ts
@@ -1,18 +1,18 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
 const client = new S3Client({
-  region: process.env.AWS_S3_LOCATION,
+  region: process.env.AWS_REGION || "",
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY || "",
-    secretAccessKey: process.env.AWS_PRIV_KEY || "",
+    secretAccessKey: process.env.AWS_SECRET_KEY || "",
   },
 });
 
 export const imageUpload = async (file: any, fileName: string) => {
   const fileBuffer = file;
-  const fileKey = `${process.env.AWS_FILE_NAME}/${fileName}-${Date.now()}`;
+  const fileKey = `${fileName}-${Date.now()}`;
   const command = new PutObjectCommand({
-    Bucket: process.env.AWS_BUCKET_NAME,
+    Bucket: process.env.AWS_S3_BUCKET,
     Key: fileKey,
     Body: fileBuffer,
     ContentType: "image",
@@ -24,5 +24,5 @@ export const imageUpload = async (file: any, fileName: string) => {
   } catch (err) {
     console.error(err);
   }
-  return `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_S3_LOCATION}.amazonaws.com/${fileKey}`;
+  return `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileKey}`;
 };
