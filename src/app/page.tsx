087136cc0761d691472690a588f966bb4401c2d4
RyanHu021 <ryanhu021@gmail.com>
2024-03-04 15:52:22 -0800

    remove test functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
 "use client";
 import AdminTable from "@/components/admin-table/admin-table";
 import SponsoredOrgTable from "@/components/sponsored-org-table/sponsored-org-table";
-import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import { imageUpload } from "@/services/image-upload";
-
-const uploadImageTest = async () => {
-  const file = new File([""], "sponsored_org_profile_picture_placeholder.png", {
-    type: "image/png",
-  });
-  const url = await imageUpload(file, "test");
-  console.log(url);
-};
 
 export default function Home() {
   const router = useRouter();
@@ -28,7 +18,6 @@ export default function Home() {
       <main>
         <h1 className="text-xl font-bold">Nonprofit Name</h1>
         <AdminTable />
-        <Button onClick={uploadImageTest}>Upload Image</Button>
       </main>
     );
   } else {
