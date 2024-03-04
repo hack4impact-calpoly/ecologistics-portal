4a6a31d35ab69104087ef25acace93ca8bb58f7b
RyanHu021 <ryanhu021@gmail.com>
2024-03-04 15:23:34 -0800

    fix: image upload cleanup

--- src/components/imageUpload.tsx
+++ src/components/imageUpload.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { imageUpload } from "@/services/imageUpload";
 import { FileUploader } from "react-drag-drop-files";
 const fileTypes = ["JPG", "PNG", "JPEG"];
 
@@ -10,14 +9,7 @@ interface ImageUploadProps {
 export default function ImageUpload({ handleChange }: ImageUploadProps) {
   return (
     <div>
-      <FileUploader
-        handleChange={(file: File) => {
-          imageUpload(file, file.name);
-          handleChange(file);
-        }}
-        name="file"
-        types={fileTypes}
-      />{" "}
+      <FileUploader handleChange={handleChange} name="file" types={fileTypes} />{" "}
     </div>
   );
 }
