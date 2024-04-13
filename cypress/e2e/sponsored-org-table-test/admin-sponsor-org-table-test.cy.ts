9ad1f12aa5a03a36534ab9d2a8039ebe523d2eee
iloo <igloo405@gmail.com>
2024-04-13 15:03:15 -0700

    added test in sponsored-org-table-test

--- cypress/e2e/sponsored-org-table-test/admin-sponsor-org-table-test.cy.ts
+++ cypress/e2e/sponsored-org-table-test/admin-sponsor-org-table-test.cy.ts
@@ -7,7 +7,9 @@ describe("Signed in", () => {
 
   it("navigate to the sponsored orgs", () => {
     // open dashboard page
-    cy.visit("http://localhost:3000/setup-organization");
+    cy.visit("http://localhost:3000/setup-organization", {
+      failOnStatusCode: false,
+    });
     cy.get(".cursor-pointer").click();
     cy.contains("Sponsored Organizations").click();
     cy.url().should("include", "/sponsored-organizations");
