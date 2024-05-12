c7bf76556864cc4ea9650fb4fcd1c06197c7c54a
joieng1 <johncieng1@gmail.com>
2024-05-11 21:25:28 -0700

    feat: create popup component

---
+++ src/components/upload-popup.tsx
@@ -0,0 +1,43 @@
+import { forwardRef, useImperativeHandle } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+interface UploadPopupProps {
+  success: boolean;
+}
+
+const UploadPopup = forwardRef(({ success }: UploadPopupProps, ref) => {
+  // Use imperativeHandle to expose openDialog function to parent
+  useImperativeHandle(ref, () => ({
+    openDialog() {
+      const trigger = document.getElementById("upload-popup-trigger");
+      if (trigger) trigger.click();
+    },
+  }));
+
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger id="upload-popup-trigger"></DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            {success ? (
+              <DialogTitle>Image Successfully Uploaded</DialogTitle>
+            ) : (
+              <DialogTitle>Image Failed to Upload</DialogTitle>
+            )}
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+});
+
+UploadPopup.displayName = "UploadPopup";
+
+export default UploadPopup;
