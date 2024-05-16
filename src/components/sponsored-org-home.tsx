943c8ba080935d69c379e6b8a844afd282927451
RyanHu021 <ryanhu021@gmail.com>
2024-05-16 09:46:00 -0700

    style: sponsored org homepage

--- src/components/sponsored-org-home.tsx
+++ src/components/sponsored-org-home.tsx
@@ -8,51 +8,6 @@ import { useRouter } from "next/navigation";
 import * as Tabs from "@radix-ui/react-tabs";
 import { Button } from "@/components/ui/button";
 
-//shadcn.ui pagination component tools:
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
-
-{
-  /* <Pagination>
-  <PaginationContent>
-    <PaginationItem>
-      <PaginationPrevious href="#" />
-    </PaginationItem>
-    <PaginationItem>
-      <PaginationLink href="#">1</PaginationLink>
-    </PaginationItem>
-    <PaginationItem>
-      <PaginationEllipsis />
-    </PaginationItem>
-    <PaginationItem>
-      <PaginationNext href="#" />
-    </PaginationItem>
-  </PaginationContent>
-</Pagination>
-
-+ import Link from "next/link"
-
-- type PaginationLinkProps = ... & React.ComponentProps<"a">
-+ type PaginationLinkProps = ... & React.ComponentProps<typeof Link>
-
-const PaginationLink = ({...props }: ) => (
-  <PaginationItem>
--   <a>
-+   <Link>
-      // ...
--   </a>
-+   </Link>
-  </PaginationItem>
-) */
-}
-
 type organizationInfo = {
   name: string;
   description: string;
@@ -84,47 +39,26 @@ export default function SponsoredHomePage() {
 
   return (
     <main className="px-14 py-12 h-screen overflow-y-auto w-full">
-      <div className="flex gap-x-6 items-center">
-        <h1 className="font-bold text-4xl">{orgInfo.name}</h1>
-        <Popup
-          name={orgInfo.name}
-          description={orgInfo.description}
-          website={orgInfo.website}
-          email={(user?.primaryEmailAddress?.emailAddress as string) || ""}
-          user={(user?.fullName as string) || ""}
-        />
+      <div className="flex items-center justify-between">
+        <div className="flex gap-x-6 items-center">
+          <h1 className="font-bold text-4xl">{orgInfo.name}</h1>
+          <Popup
+            name={orgInfo.name}
+            description={orgInfo.description}
+            website={orgInfo.website}
+            email={(user?.primaryEmailAddress?.emailAddress as string) || ""}
+            user={(user?.fullName as string) || ""}
+          />
+        </div>
+        <Button
+          onClick={() => router.push("/reimbursements")}
+          className="bg-orange-500 text-sm place-self-end"
+        >
+          Request Reimbursement
+        </Button>
       </div>
       <div className="flex flex-col mt-8">
-        <Tabs.Root defaultValue="all">
-          <Tabs.List className="flex gap-x-2 border-b border-gray-500">
-            <Tabs.Trigger
-              className="text-[15px] px-1 pb-1 font-[500] hover:text-orange-500 data-[state=active]:text-orange-500 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current outline-none cursor-pointer"
-              value="all"
-            >
-              All Requests
-            </Tabs.Trigger>
-            <Tabs.Trigger
-              className="text-[15px] px-1 pb-1 font-[500] hover:text-orange-500 data-[state=active]:text-orange-500 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current outline-none cursor-pointer"
-              value="archived"
-            >
-              Archived Requests
-            </Tabs.Trigger>
-          </Tabs.List>
-
-          <div className="flex flex-col mt-4">
-            <Button
-              onClick={() => router.push("/reimbursements")}
-              className="bg-orange-500 text-[16px] font-[600] place-self-end"
-            >
-              Request Reimbursement
-            </Button>
-            <Tabs.Content value="all">
-              <SponsoredOrgTable />
-            </Tabs.Content>
-
-            <Tabs.Content value="archived"></Tabs.Content>
-          </div>
-        </Tabs.Root>
+        <SponsoredOrgTable />
       </div>
     </main>
   );
