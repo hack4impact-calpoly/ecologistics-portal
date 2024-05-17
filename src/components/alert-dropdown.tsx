f11eda6fcd33571c3248590a331dac2598c539c8
Peter Chinh <peterchinh1018@gmail.com>
2024-05-17 14:22:59 -0700

    feat: Added functionality for fetching and deleting user's alerts

--- src/components/alert-dropdown.tsx
+++ src/components/alert-dropdown.tsx
@@ -1,7 +1,14 @@
 "use client";
-
-import { useState } from "react";
-import { BellIcon } from "@radix-ui/react-icons";
+import useSWR from "swr";
+import {
+  JSXElementConstructor,
+  PromiseLikeOfReactNode,
+  ReactElement,
+  ReactNode,
+  ReactPortal,
+  useState,
+} from "react";
+import { BellIcon, Cross2Icon } from "@radix-ui/react-icons";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
@@ -11,18 +18,33 @@ import {
   DropdownMenuSeparator,
 } from "components/ui/dropdown-menu";
 import NewUserButton from "./user-button";
-import { useUser } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 // Alert component on header
-export default function Alert() {
+export default function AlertIcon() {
   const { isSignedIn } = useUser();
+  const { userId } = useAuth();
   const [hasNewUpdates, setHasNewUpdates] = useState(true); // Assume there are new updates initially
 
-  const statusUpdates = [
-    { id: 1, text: "Request A approved" },
-    { id: 2, text: "Request B approved" },
-    { id: 3, text: "Request C denied" },
-  ];
+  const { data, error, mutate } = useSWR(
+    isSignedIn ? `/api/alert/${userId}` : null,
+    (...args) => fetch(...args).then((res) => res.json()),
+  );
+
+  const handleDeleteAlert = async (alertId: string) => {
+    try {
+      const res = await fetch(`api/alert/${userId}?id=${alertId}`, {
+        method: "DELETE",
+      });
+      if (!res.ok) {
+        throw new Error("Failed to fetch reimbursement");
+      }
+      mutate();
+    } catch (error) {
+      console.log(`error: ${error}`);
+    }
+  };
 
   const handleDropdownOpen = () => {
     setHasNewUpdates(false); // Resets the new updates state when dropdown is opened
@@ -32,9 +54,9 @@ export default function Alert() {
     return (
       <div className="z-10">
         <div className="flex space-x-4">
-          <DropdownMenu>
+          <DropdownMenu onOpenChange={handleDropdownOpen}>
             <DropdownMenuTrigger asChild>
-              <button onClick={handleDropdownOpen}>
+              <button>
                 <div className="relative">
                   <BellIcon className="w-6 h-6" />
                   {hasNewUpdates && <span className="badge"></span>}
@@ -44,11 +66,46 @@ export default function Alert() {
             <DropdownMenuContent className="right-0">
               <DropdownMenuLabel>Recent Updates</DropdownMenuLabel>
               <DropdownMenuSeparator />
-              {statusUpdates.map((statusUpdate) => (
-                <DropdownMenuItem key={statusUpdate.id}>
-                  {statusUpdate.text}
-                </DropdownMenuItem>
-              ))}
+              {data &&
+                !error &&
+                data.map(
+                  (
+                    alert: {
+                      title: string;
+                      description: string;
+                      _id: string;
+                    },
+                    index: number,
+                  ) => (
+                    <DropdownMenuItem key={index}>
+                      <div className="flex flex-row w-full justify-between items-center">
+                        <div>
+                          <div>{alert.title}</div>
+                          <div>
+                            {alert.description
+                              .split("\n")
+                              .map((line, index) => (
+                                <div key={index}>{line}</div>
+                              ))}
+                          </div>
+                        </div>
+                        <div>
+                          <Button
+                            variant="ghost"
+                            size="icon"
+                            className="w-6 h-6 flex justify-center items-center rounded-lg ml-3 hover:bg-red-400"
+                            onClick={(e) => {
+                              e.stopPropagation(); // Prevent event propagation to parent elements
+                              handleDeleteAlert(alert._id); // Call delete handler function
+                            }}
+                          >
+                            <Cross2Icon />
+                          </Button>
+                        </div>
+                      </div>
+                    </DropdownMenuItem>
+                  ),
+                )}
             </DropdownMenuContent>
           </DropdownMenu>
           <NewUserButton />
