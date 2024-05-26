1b1c40dde564cf129300971514d194eb4383f04c
wbrandon25 <legobw1018@gmail.com>
2024-05-26 14:13:22 -0700

    style: clerk sign in component button to orange

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,10 +2,16 @@ import FullscreenSpinner from "@/components/fullscreen-spinner";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
+  const signInAppearance = {
+    elements: {
+      formButtonPrimary: "bg-orange-500 hover:bg-orange-600",
+    },
+  };
+
   return (
     <div className="w-screen h-screen fixed top-0 left-0 z-0">
       <div className="w-full h-screen flex justify-center items-center">
-        <SignIn />
+        <SignIn appearance={signInAppearance} />
       </div>
     </div>
   );
